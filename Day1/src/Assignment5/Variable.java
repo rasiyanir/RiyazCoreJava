a4e8fad284779d3c9d7c6b39c74b9fe61623c586
riyaz rasiyani <riyazr8016@gmail.com>
2019-06-11 16:26:11 -0500

    commitDay1

---
+++ Day1/src/Assignment5/Variable.java
@@ -0,0 +1,12 @@
+package Assignment5;
+
+public class Variable {
+	public static void main(String[] args) {
+		int intVal = 20;
+		float 	floatVal = 250.5f;
+		double doubleVal = 2500.5;
+		boolean boolVal = true;
+		
+		System.out.println("Integer\t: " + intVal + "\nFloat\t: " + floatVal + "\nDouble: " + doubleVal + "\nBoolean\t: " + boolVal );
+	}
+}
