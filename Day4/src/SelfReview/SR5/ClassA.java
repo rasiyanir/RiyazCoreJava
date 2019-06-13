2bf78ddc50b9e69da1fa8a4a68648de42e8da928
riyaz rasiyani <riyazr8016@gmail.com>
2019-06-12 22:43:37 -0500

    commit

--- Day4/src/SelfReview/SR5/ClassA.java
+++ Day4/src/SelfReview/SR5/ClassA.java
@@ -5,14 +5,14 @@ public class ClassA {
 	}
 	public void methodTwo(int i){
 	}
-	public static void methodThree(int i){
+	public void methodThree(int i){
 	}
 	public static void methodFour(int i){
 	}
 }
 
 class ClassB extends ClassA{
-	public static void methodOne(int i){
+	public  void methodOne(int i){
 	}
 	public void methodTwo(int i){
 	}
